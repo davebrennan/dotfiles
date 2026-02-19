28769293fadbdc12ce0022a1f39305ab7a59978f
DaveBrennan <davethebabe713@gmail.com>
2026-02-18 18:37:14 -0700

    Added nvim directory

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,2 @@
+--vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", {desc="Open Parent Directory in Oil"})
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,37 @@
+return {
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+
+	    require('telescope').setup{
+
+	    	pickers = {
+    			find_files = {
+      			theme = "ivy",
+    			}
+  	    	}
+	    }
+
+
+
+
+	    local builtin = require('telescope.builtin')
+	    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+	    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	    
+	    vim.keymap.set("n", "<space>en", function()
+		    require('telescope.builtin').find_files {
+			    cwd = vim.fn.stdpath("config")
+
+		    }
+	    end)
+
+    end
+    
+}
